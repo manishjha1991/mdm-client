b7251145d506b68691d942de8017cb949d071792
B0209123 <B0209123@LTB0209123s-MacBook-Pro.local>
2019-02-10 22:59:10 +0530

    add fixes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,12 +2,9 @@ import React, { Component } from "react";
 import {
   Card,
   CardImg,
-  CardBody,
-  CardTitle,
+  Button,
   ListGroup,
   ListGroupItem,
-  CardText,
-  CardLink,
   Col
 } from "reactstrap";
 import "./Home.css";
@@ -29,21 +26,53 @@ export default class Home extends Component {
       device: getAllStoreJson.result
     });
   }
+  walpaper =
+    "https://s3-ap-southeast-1.amazonaws.com/bsy/iportal/images/airtel-logo-white-text-horizontal.jpg";
   render() {
     return this.state.device.map((note, i) => (
       <div className="Home">
         <Col sm="4">
-          <Card style={{ width: "18rem" }}>
-            <CardImg variant="top" src="holder.js/100px180?text=Image cap" />
-            <CardBody>
-              <CardTitle>{note.circleId}</CardTitle>
-              <CardTitle>{note.deviceId}</CardTitle>
-              <CardText>{note.groupName}</CardText>
-            </CardBody>
+          <Card style={{ width: "25rem" }}>
+            <CardImg
+              style={{ width: "100%" }}
+              variant="top"
+              src={this.walpaper}
+            />
             <ListGroup className="list-group-flush">
+              <ListGroupItem>
+                <b>Circle ID:</b>
+                {note.circleId}
+              </ListGroupItem>
+              <ListGroupItem>
+                <b>Store ID:</b>:{note.storeId}
+              </ListGroupItem>
+              <ListGroupItem>
+                <b>Device ID:</b>
+                {note.deviceId}
+              </ListGroupItem>
+              <ListGroupItem>
+                <b>Group Name:</b>
+                {note.groupName}
+              </ListGroupItem>
+              <ListGroupItem>
+                <b>Apps</b>
+                <ListGroupItem>
+                  {note.selectedApps.map(app => {
+                    return <Button bsStyle="success">{app.appName}</Button>;
+                  })}
+                </ListGroupItem>
+              </ListGroupItem>
               <ListGroupItem>{}</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+              <ListGroupItem>
+                <b>Browser</b>
+              </ListGroupItem>
+              <ListGroupItem>
+                {note.selectedBrowser.map(browser => {
+                  return (
+                    <Button bsStyle="success">{browser.browserName}</Button>
+                  );
+                })}
+              </ListGroupItem>
             </ListGroup>
           </Card>
         </Col>
